1ce536c543da0a65315866aa5c8b2f4c087fc12e
Lorenzo Rimei <126676473+Riimoo30@users.noreply.github.com>
2023-03-21 10:28:30 +0100

    Add files via upload

---
+++ Constructors/_2/Student.java
@@ -0,0 +1,33 @@
+package javaoop.Constructors._2;
+
+public class Student {
+    String name;
+    Integer age;
+
+    public Student(String name, Integer age){
+        System.out.println("Constructing");
+        this.name = name;
+        this.age = age;
+        System.out.println("Name = " + name + ", Age = " +age);
+        System.out.println("Constructed\n");
+
+
+    }
+
+    public Student(Integer age){
+        System.out.println("Constructor 2");
+        this.age = age;
+        System.out.println(age);
+
+    }
+
+    public Student(String name){
+        System.out.println("constructor 3");
+        this.name = name;
+        System.out.println(name);
+    }
+
+    public Student(){
+        System.out.println("empty");
+    }
+}
