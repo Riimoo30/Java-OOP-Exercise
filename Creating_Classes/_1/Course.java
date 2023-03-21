f52f647abf4d9019f067f16c33734e36f5d631bc
Lorenzo Rimei <126676473+Riimoo30@users.noreply.github.com>
2023-03-21 10:28:17 +0100

    Add files via upload

---
+++ Creating_Classes/_1/Course.java
@@ -0,0 +1,19 @@
+package javaoop.Creating_Classes._1;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Course {
+    String courseName;
+    int maxStudents;
+    int qualityRatingOutOf10;
+   ;
+
+   public  List<Student> students = new ArrayList<>();
+
+
+    }
+
+
+
+
---
+++ Creating_Classes/_1/Student.java
@@ -0,0 +1,20 @@
+package javaoop.Creating_Classes._1;
+
+import java.util.Arrays;
+public class Student {
+    String name;
+    Integer age;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+
+    }
+}
+
+
+
+
